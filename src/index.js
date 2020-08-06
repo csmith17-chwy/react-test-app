86814e7c3667e741164b954b5c1891ab26359050
Colin Smith <csmith17@chewy.com>
2020-08-06 12:55:58 -0400

    conditional rendering

--- src/index.js
+++ src/index.js
@@ -2,25 +2,34 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
-const lakeList = [
-  { id: "1", name: "Echo Lake", trailhead: "Echo" },
-  { id: "2", name: "Lake George", trailhead: "George" },
-  { id: "3", name: "Lake Tahoe", trailhead: "Tahoe" },
-  { id: "4", name: "Crater Lake", trailhead: "Crater" },
-  { id: "5", name: "Lake Placid", trailhead: "Placid" },
-];
-
-function App({ lakes }) {
+function Lake({ name }) {
   return (
     <div>
-      {lakes.map((lake) => (
-        <div key={lake.id}>
-          <h2>lake.name</h2>
-          <p>Accessed by: {lake.trailhead}</p>
-        </div>
-      ))}
+      <h1>Visit {name}!</h1>
     </div>
   );
 }
 
-ReactDOM.render(<App lakes={lakeList} />, document.getElementById("root"));
+function SkiResort({ name }) {
+  return (
+    <div>
+      <h1>Visit {name}!</h1>
+    </div>
+  );
+}
+
+function App(props) {
+  return (
+    <div>
+      {props.season === "summer" ? (
+        <Lake name="Smith Lake" />
+      ) : props.season === "winter" ? (
+        <SkiResort name="Forge Mountain" />
+      ) : (
+        <h1>Come back in the winter or summer!</h1>
+      )}
+    </div>
+  );
+}
+
+ReactDOM.render(<App season="winter" />, document.getElementById("root"));
