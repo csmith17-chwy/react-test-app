591accd34be535b0b5eb7dc151deb0340b163d4b
Colin Smith <csmith17@chewy.com>
2020-08-06 12:38:39 -0400

    rendering lists

--- src/index.js
+++ src/index.js
@@ -2,18 +2,22 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
-function Lake({ name }) {
-  return <h1>{name}</h1>;
-}
+const lakeList = [
+  "Echo Lake",
+  "Lake George",
+  "Lake Tahoe",
+  "Crater Lake",
+  "Lake Placid",
+];
 
-function App() {
+function App({ lakes }) {
   return (
-    <div>
-      <Lake name="Lake Tahoe" />
-      <Lake name="Lake George" />
-      <Lake name="Lake Placid" />
-    </div>
+    <ul>
+      {lakes.map((lake) => (
+        <li>{lake}</li>
+      ))}
+    </ul>
   );
 }
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<App lakes={lakeList} />, document.getElementById("root"));
