12fa0f603ac2c805b8734c83f4bacb03617d32b2
Colin Smith <csmith17@chewy.com>
2020-08-06 16:08:59 -0400

    fetch with useEffect

--- src/index.js
+++ src/index.js
@@ -2,30 +2,28 @@ import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
+function GitHubUser({ login }) {
+  const [data, setData] = useState(null);
+  useEffect(() => {
+    fetch(`https://api.github.com/users/${login}`)
+      .then((res) => res.json())
+      .then(setData)
+      .catch(console.error);
+  }, []);
+
+  if (data) {
+    return (
+      <div>
+        <h1>{data.login}</h1>
+        <img src={data.avatar_url} alt="" width={100} />
+      </div>
+    );
+  }
+  return null;
+}
+
 function App() {
-  const [val, setVal] = useState("");
-  const [val2, setVal2] = useState("");
-
-  useEffect(() => {
-    console.log(`field 1: ${val}`);
-  }, [val]);
-
-  useEffect(() => {
-    console.log(`field 2: ${val2}`);
-  }, [val2]);
-  return (
-    <>
-      <label>
-        Favorite Phrase:
-        <input value={val} onChange={(e) => setVal(e.target.value)} />
-      </label>
-      <br />
-      <label>
-        Second Favorite Phrase:
-        <input value={val2} onChange={(e) => setVal2(e.target.value)} />
-      </label>
-    </>
-  );
+  return <GitHubUser login="csmith17-chwy" />;
 }
 
 ReactDOM.render(<App />, document.getElementById("root"));
