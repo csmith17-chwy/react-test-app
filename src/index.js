ada95aa47a5ed03fc2331f6ffa4674b7d98c13c4
Colin Smith <csmith17@chewy.com>
2020-08-06 12:29:24 -0400

    init create-react-app

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+
+function Hello({ library, message, number }) {
+  return (
+    <div>
+      <h1>Welcome to {library}!</h1>
+      <p>{message}</p>
+      <p>{number} Props Total</p>
+    </div>
+  );
+}
+
+ReactDOM.render(
+  <Hello library="React" message="Hola Class" number={3} />,
+  document.getElementById("root")
+);
